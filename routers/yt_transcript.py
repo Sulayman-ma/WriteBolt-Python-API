64e1d877eb65b6b5b31746f64a900361f992ac56
sulayman-ma <sulayman.abba@gmail.com>
2024-08-25 10:52:07 +0100

    requirements and transcript endpoint fix

--- routers/yt_transcript.py
+++ routers/yt_transcript.py
@@ -2,7 +2,6 @@
 
 from typing import Any, Annotated
 from youtube_transcript_api import YouTubeTranscriptApi
-from youtube_transcript_api.formatters import JSONFormatter
 
 from fastapi import APIRouter, Query, Response
 
@@ -11,25 +10,24 @@ router = APIRouter(
     prefix="/transcript",
     tags=['transcript'],
     responses={
-        404: {"message": "Transcript not found"},
         400: {"message": "Please include a valid video ID"}
     }
 )
 
 """Path Operations"""
-@router.get("/", status_code=200, response_model=list[dict[str, Any]])
+@router.get(path="/", status_code=200)
 async def get_transcript(
     video_id: Annotated[str, Query(title="Video ID", min_length=1)]
 ) -> Response:
     """Takes a YouTube video's ID and retrieves the transcript from it."""
 
     try:
-        transcript = YouTubeTranscriptApi.get_transcript(
+        transcript = await YouTubeTranscriptApi.get_transcript(
             video_id=video_id, 
             languages=['en', 'hi', 'ur', 'es', 'fr', 'de', 'zh-Hans', 'zh-Hant', 'ar', 'pt', 'it', 'ja', 'ko']
         )
-        formatter = JSONFormatter()
-        json_formatted = formatter.format_transcript(transcript)
+        # formatter = JSONFormatter()
+        # json_formatted = formatter.format_transcript(transcript)
 
         return transcript
 
